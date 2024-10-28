a68ca5132029fd4ab27b4839e138630253c43e34
Johannes Edelstam <johannes@confetti.events>
2024-10-28 09:59:43 +0100

    Add code layer

--- keyboards/keebio/iris/keymaps/jede/keymap.c
+++ keyboards/keebio/iris/keymaps/jede/keymap.c
@@ -5,6 +5,7 @@ extern keymap_config_t keymap_config;
 #define _QWERTY 0
 #define _LOWER 1
 #define _RAISE 2
+#define _CODE 5
 #define _ADJUST 16
 
 
@@ -14,9 +15,13 @@ enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
   RAISE,
+  CODE,
   ADJUST,
   CONFETTI,
   SMALLDR,
+  PNUM,
+  JS_ARW,
+  JS_LOG
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -24,9 +29,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT(
      KC_ESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-SFT_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-     KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, CTL_T(KC_ESC),      KC_BSPC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                                    GUI_T(KC_SPC), LOWER,   ALT_T(KC_ENT),                    KC_SPC,  RAISE,   ALT_T(KC_ENT)
+    KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+     KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, CODE,      KC_BSPC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                 GUI_T(KC_SPC), LOWER,   ALT_T(KC_ENT),                    KC_SPC,  RAISE,   ALT_T(KC_ENT)
   ),
 
 /*  [_LOWER] = LAYOUT(
@@ -46,24 +51,32 @@ SFT_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                        
   ),*/
 
   [_LOWER] = LAYOUT(
-     _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
+     G(LCTL(KC_Q)), KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
      OSM(MOD_HYPR), LGUI(KC_NUBS), LCTL(LGUI(KC_ENT)), _______, KC_LCBR, KC_RCBR,            KC_LBRC, KC_RBRC, _______, _______, _______, _______,
-     _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,                            A(KC_LBRC), A(KC_QUOT), A(KC_SCLN), _______, _______, _______,
-     _______, KC_TILD, _______, _______, KC_SPC,  KC_BSPC, _______,          _______, KC_BSLS, KC_PIPE, KC_SLSH, _______, LGUI(KC_BSLS), _______,
+     _______, G(KC_ESC), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,                            A(KC_LBRC), A(KC_QUOT), A(KC_SCLN), _______, _______, LGUI(KC_BSLS),
+     KC_SLEP, KC_GRV, _______, _______, KC_SPC,  KC_BSPC, _______,           KC_DEL, KC_BSLS, KC_PIPE, G(A(KC_LEFT)), G(A(KC_RGHT)), _______, _______,
                                     _______, _______, _______,                   _______, _______, _______
   ),
 
   [_RAISE] = LAYOUT(
      KC_GRV, KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,
-     _______, KC_ACL0, KC_ACL1, KC_ACL2, KC_BTN2, KC_BTN1,                            _______, _______, _______, _______, _______, _______,
-     _______, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,                            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_BRMD, KC_BRMU,
-     _______, KC_GRV, _______, _______, KC_WH_D, KC_WH_U, _______,          _______, KC_MRWD, KC_MPLY, KC_MFFD, KC__MUTE, KC__VOLDOWN, KC__VOLUP,
+     _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______,
+     _______, _______, KC_PGDN, KC_PGUP, _______, _______,                KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_BRMD, KC_BRMU,
+     _______, KC_TILD, _______, _______, KC_WH_D, KC_WH_U, _______,          _______, KC_MRWD, KC_MPLY, KC_MFFD, KC__MUTE, KC__VOLDOWN, KC__VOLUP,
+                                    _______, _______, _______,                   _______, _______, _______
+  ),
+
+  [_CODE] = LAYOUT(
+     _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
+     _______, _______, _______, _______, KC_LCBR, KC_RCBR,                            KC_LPRN, KC_RPRN, _______, _______, _______, _______,
+     _______, _______, _______, _______, KC_LBRC, KC_RBRC,                            KC_LT,     KC_GT, _______, _______, _______, _______,
+     _______, _______, _______,  JS_LOG, _______, _______, _______,          _______, JS_ARW, _______, _______, _______, _______, _______,
                                     _______, _______, _______,                   _______, _______, _______
   ),
 
   [_ADJUST] = LAYOUT(
      RESET, RGB_TOG, RGB_MOD, _______, _______, _______,                              _______, _______, _______, _______, _______, _______,
-     RGB_HUD, RGB_HUI, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
+     RGB_HUD, RGB_HUI, _______, _______, _______, MEH(KC_T),                          MEH(KC_Y), _______, _______, _______, PNUM, _______,
      RGB_SAD, RGB_SAI, SMALLDR, LGUI(KC_NUBS), LCTL(LGUI(KC_ENT)), _______,           G(S(KC_LEFT)), _______, _______, G(S(KC_RGHT)), _______, _______,
      RGB_VAD, RGB_VAI, _______, CONFETTI, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______,
                                     _______, _______, _______,                   _______, _______, _______
@@ -98,6 +111,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
+    case CODE:
+      if (record->event.pressed) {
+        layer_on(_CODE);
+      } else {
+        layer_off(_CODE);
+      }
+      return false;
+      break;
     case ADJUST:
       if (record->event.pressed) {
         layer_on(_ADJUST);
@@ -114,6 +135,21 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case CONFETTI:
       if (record->event.pressed) {
         SEND_STRING("5569646465");
+      }
+      break;
+    case PNUM:
+      if (record->event.pressed) {
+        SEND_STRING("19XXXXXXXXXX");
+      }
+      break;
+    case JS_ARW:
+      if (record->event.pressed) {
+        SEND_STRING("=> ");
+      }
+      break;
+    case JS_LOG:
+      if (record->event.pressed) {
+        SEND_STRING("console.log(55555)");
       }
       break;
   }
